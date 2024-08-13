a6e1cbafcb2c5c80a6fa543b738491caf1b283eb
LDAWoo <leducanhvu3006@gmail.com>
2024-08-13 17:38:08 +0700

    add sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "@/providers/theme.provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ModalProvider } from "@/providers/model.provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Plura",
-    description: "All in one Agenry Solutiion",
+    description: "All in one Agency Solution",
 };
 
 export default function RootLayout({
@@ -16,10 +18,16 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                <body className={inter.className}>{children}</body>
-            </ThemeProvider>
+        <html lang="en" suppressHydrationWarning>
+            <head>{/* Other head elements, if needed */}</head>
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    <ModalProvider>
+                        {children}
+                        <Toaster />
+                    </ModalProvider>
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
